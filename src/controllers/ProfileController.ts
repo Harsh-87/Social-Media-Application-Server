1a4b4e15114c5ce3e8dc4c7d42d3db2a2b409598
Harsh Kothari <harsh.kothari@hyperverge.co>
2021-01-15 20:45:51 +0530

    Follow, Unfollow requests added to profileController

--- src/controllers/ProfileController.ts
+++ src/controllers/ProfileController.ts
@@ -19,3 +19,51 @@ exports.getAllProfiles = (req, res, next) => {
         }, (err) => next(err))
         .catch((err) => next(err));
 }
+
+exports.followRequest = async function (req, res, next) {
+    await User.findById(req.user['_id'])
+        .then(async (user) => {
+            user.following.push(req.params.id);
+            await user.save();
+        }, (err) => next(err))
+        .catch((err) => next(err));
+
+    await User.findById(req.params.id)
+        .then(async (user) => {
+            user.followers.push(req.user['_id']);
+            await user.save();
+        }, (err) => next(err))
+        .catch((err) => next(err));
+
+    await User.find({})
+        .then((users) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(users);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+}
+
+exports.unfollowRequest = async function (req, res, next) {
+    await User.findOneAndUpdate({ _id: req.user['_id'] }, { $pull: { following: { $in: [req.params.id] } } }, { new: true }, function (err, user) {
+        if (err) {
+            console.log(err);
+        }
+    }, (err) => next(err))
+        .catch((err) => next(err));
+
+    await User.findOneAndUpdate({ _id: req.params.id }, { $pull: { followers: { $in: [req.user['_id']] } } }, { new: true }, function (err, user) {
+        if (err) {
+            console.log(err);
+        }
+    }, (err) => next(err))
+        .catch((err) => next(err));
+
+    await User.find({})
+        .then((users) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(users);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+}
--- src/routes/connectionRouter.ts
+++ src/routes/connectionRouter.ts
@@ -1,54 +1,10 @@
 import express = require('express');
-var router = express.Router();
-var authenticate = require('../authenticate');
-var User = require("../models/user");
+const router = express.Router();
+const authenticate = require('../authenticate');
+const profile_controller = require("../controllers/ProfileController");
 
-router.get('/:id/follow', authenticate.verifyUser, async function (req, res, next) {
-    await User.findById(req.user['_id'])
-        .then(async (user) => {
-            user.following.push(req.params.id);
-            await user.save();
-        }, (err) => next(err))
-        .catch((err) => next(err));
+router.get('/:id/follow', authenticate.verifyUser, profile_controller.followRequest);
 
-    await User.findById(req.params.id)
-        .then(async (user) => {
-            user.followers.push(req.user['_id']);
-            await user.save();
-        }, (err) => next(err))
-        .catch((err) => next(err));
-
-    await User.find({})
-        .then((users) => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json(users);
-        }, (err) => next(err))
-        .catch((err) => next(err));
-});
-
-router.get('/:id/unfollow', authenticate.verifyUser, async function (req, res, next) {
-    await User.findOneAndUpdate({ _id: req.user['_id'] }, { $pull: { following: { $in: [req.params.id] } } }, { new: true }, function (err, user) {
-        if (err) {
-            console.log(err);
-        }
-    }, (err) => next(err))
-        .catch((err) => next(err));
-
-    await User.findOneAndUpdate({ _id: req.params.id }, { $pull: { followers: { $in: [req.user['_id']] } } }, { new: true }, function (err, user) {
-        if (err) {
-            console.log(err);
-        }
-    }, (err) => next(err))
-        .catch((err) => next(err));
-
-    await User.find({})
-        .then((users) => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json(users);
-        }, (err) => next(err))
-        .catch((err) => next(err));
-});
+router.get('/:id/unfollow', authenticate.verifyUser, profile_controller.unfollowRequest);
 
 module.exports = router;
